b66b8bbebbfb06df46adad60f40f20398ba409cd
user-b <visitveera8@gmail.com>
2018-02-16 07:40:56 +0000

    added string_operations

--- src/string_operations.c
+++ src/string_operations.c
@@ -24,14 +24,6 @@ char *s[] =
 };
 
 
-int main(void)
-{
-int i;
-char *s[] =
-{
-"Git tutorials",
-"zippyops"
-};
 for (i = 0; i < 2; ++i)
 printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
 
